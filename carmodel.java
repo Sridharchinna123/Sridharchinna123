228e0c71e3859c140d33dd8e47ba6be22270cde8
Sridharchinna123 <108472779+Sridharchinna123@users.noreply.github.com>
2022-11-10 19:53:05 +0530

    Add files via upload

---
+++ carmodel.java
@@ -0,0 +1,19 @@
+package sridhar123;
+
+public class carmodel{
+	public static void main(string[] args) {
+	                 // TODO Auto-generated method stub 
+     enum car{dodge(1970),mustangGT(2022),Thar(2021); 
+	                 private int value;   
+	                 private car(int value){   
+	                 this.value=value;   
+	                 }   
+	                 } 
+	                 System.out.println("    Car Name\tModel Year"); 
+	                 for(car c : car.values()) 
+	                 System.out.println("    "+c+"\t"+c.value); 
+	         } 
+	]
+			
+	  
+	
